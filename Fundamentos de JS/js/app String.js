ab67cc865a95da79aa34afd0ccce5e6f8ab44688
oscarali1985 <oscarali1985@hotmail.com>
2020-01-27 23:02:45 -0400

    Agregando Js

---
+++ Fundamentos de JS/js/app String.js
@@ -0,0 +1,107 @@
+
+// Primera Prueba
+let nombre = prompt("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+/* alt 96 para `` */
+
+document.getElementById('app').innerHTML = ` Bienvenidos a 4Geeks ${nombre} tu tienes ${edad} haz iniciado el curso de programacion full-stack con nosotros`
+
+
+console.log('El nombre ingresado es '+nombre+' y su edad '+ edad);
+//  Prueba
+confirm ('Desea borrar el articulo');
+
+
+
+alert('Hola');
+
+//  Prueba
+
+/* con document traemos todo DOM*/
+document
+/*Nos trae el primero que encuentre*/
+document.querySelector('h1');
+/* Nos trae todos los elementos */
+document.querySelectorAll('h2');
+
+//  Prueba
+
+/*Arreglo */
+console.log([1,2,3]);
+console.error("Hola Mundo");
+console.warn("Hola Mundo");
+
+/**   Otro mas */
+console.error("Hola Mundo");
+console.warn("Hola Mundo");
+console.time('Hola');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.warn('Hay un error');console.timeEnd('Hola');
+/**   Otro mas */
+
+let carrito;
+carrito="libro";
+carrito="zapato";
+
+console.log(carrito);
+
+//  Prueba
+
+/*Crear Variables */
+
+let primerNombre = 'Juan' //camilcase Usar preferiblmente
+let primer_nombre = 'Juan' //undercore
+let PrimerNombre = 'Juan' //pascalcase
+
+console.log(primerNombre,primer_nombre);
+
+
+//  Prueba
+
+let aprendiendo = "aprendiendo";
+
+console.log(aprendiendo.length);
+
+//  Prueba
+
+let tecnologia = 'JavaScript';
+
+console.log(tecnologia.concat('es genial'));
+
+//  Prueba
+let tecnologia = 'JavaScript';
+
+console.log(tecnologia.toUpperCase());
+console.log(tecnologia.toLowerCase());
+
+//  Prueba
+/** Buscar en una cadena */
+
+let mensaje= 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.';
+
+console.log(mensaje.indexOf('adipisicing'));
+
+/** Tomar y buscar desde una posicion hasta */
+
+let mensaje= 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.';
+
+console.log(mensaje.substring(0,11));
+/**porcion  */
+console.log(mensaje.slice(11));
+console.log(mensaje.slice(-8));
+/**Convierte el string en arreglo dentro del  */
+console.log(mensaje.split(''));
+
+/* Reemplaza el valor 1 x 2*/
+console.log(mensaje.replace('dolor','gustar'));
+
+
+/*Esta dentro */
+console.log(mensaje.includes('dolor'));
+
+/*Repite mensaje */
+console.log(mensaje.repeat(22));
+
+/**Numero */
+/* */
+
+
+/* */
---
+++ Fundamentos de JS/js/app.js
@@ -0,0 +1,35 @@
+const numero1 = 30, 
+      numero2=15,
+      numero3=20.20,
+      numero4=2,
+      numero5=3;
+    
+let resultado1,
+    resultado2,
+    resultado3,
+    resultado4,
+    resultado5;
+
+//sumar
+resultado1 = numero1+numero2;
+
+//restar
+resultado2 = numero1-numero2;
+
+//Multiplicar
+resultado3 = numero3*numero2;
+
+//dividir
+resultado4 = numero3/numero5;
+
+//modulo
+resultado5 = numero5 % numero4;
+
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+
+
